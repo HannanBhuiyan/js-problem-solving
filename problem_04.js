14f307c4af7ece3d9bad2ba3e08119aa8eccc2b2
HannanBhuiyan <hannan.diuh@gmail.com>
2023-03-21 01:25:14 +0600

    upload

---
+++ problem_04.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7. 
+
+a = 5;
+b = 6;
+c = 7
+let s = (a + b + c) / 2
+
+let areaOfTriangle = Math.sqrt( s*(s-a)*(s-b)*(s-c))
+console.log(areaOfTriangle.toFixed(2))
