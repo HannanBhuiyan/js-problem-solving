9476d936f70c9068d738905ebbf817f956153c3f
HannanBhuiyan <hannan.diuh@gmail.com>
2023-03-21 18:21:26 +0600

    upload

--- problem_05.js
+++ problem_05.js
@@ -3,6 +3,12 @@ window.addEventListener('load', function() {
     let textNode = elements.childNodes[0]
     let text = textNode.data;
 
+    // console.log(text.length)
+    // console.log("last" + text[text.length - 1])
+    // console.log(text.substring(0, text.length -1))
+    // text = text[text.length - 1] + text.substring(0, text.length - 1);
+    // console.log(text)
+
     setInterval(() => {
         text = text[text.length - 1] + text.substring(0, text.length - 1);
         textNode.data = text;
--- problem_06.js
+++ problem_06.js
@@ -1,1 +1,14 @@
+// 6. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
  
+
+function isLeapYear(year){
+    if((year % 4 === 0) || (year % 400 === 0) && (year % 100 !== 0)){
+        console.log(`${year} is a leap year`)
+    }
+    else {
+        console.log(`${year} is not a leap year`)
+    }
+    
+}
+
+isLeapYear(2013)
