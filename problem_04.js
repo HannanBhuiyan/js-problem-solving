b4bcc476d4785199611f0dcd130d3361d659e4cf
HannanBhuiyan <hannan.diuh@gmail.com>
2023-03-21 23:39:37 +0600

    problem solve 7

--- problem_04.js
+++ problem_04.js
@@ -1,4 +1,4 @@
-// Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7. 
+// 4 Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7. 
 
 a = 5;
 b = 6;
